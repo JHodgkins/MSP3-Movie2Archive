d73a4c3b9663953b2e61bc3e480c01687df66e79
JHodgkins <jhodgkins78@gmail.com>
2022-07-09 08:57:34 +0000

    users model imported

--- media2archive/routes.py
+++ media2archive/routes.py
@@ -1,9 +1,11 @@
 """
 Import flask to habdle routes and render_template to handle template files.
 import app and db from __init__ file
+mport Users from the models file
 """
 from flask import render_template
 from media2archive import app, db
+from media2archive.models import Users
 
 
 @app.route("/")
