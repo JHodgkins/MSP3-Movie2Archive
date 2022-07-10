bc2aa08cae7c0244a97a0cc8eff48b44cda2db56
JHodgkins <jhodgkins78@gmail.com>
2022-07-10 18:13:34 +0000

    code: movielookup schema constructed inc FK and added

--- movie2archive/models.py
+++ movie2archive/models.py
@@ -14,6 +14,8 @@ class User(db.Model):
     user_name = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(260), nullable=False)
     join_date = db.Column(db.Date, nullable=False)
+    users = db.relationship(
+        "Movielookup", backref="user", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         """
@@ -22,12 +24,14 @@ class User(db.Model):
         return f"#{self.user_id} | Username: {self.username} | Fistname: {self.first_name} | Lastname: {self.last_name} | Join date: {self.join_date} "
 
 
-class MediaType(db.Model):
+class Media(db.Model):
     """
     Schema for the media type table.
     """
     media_type_id = db.Column(db.Integer, primary_key=True)
     media_type = db.Column(db.String(20), nullable=False)
+    mediatypes = db.relationship(
+        "Movielookup", backref="media", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         """
@@ -42,12 +46,14 @@ class Location(db.Model):
     """
     location_id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String(20), nullable=False)
+    locations = db.relationship(
+        "Movielookup", backref="location", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         """
         Represent each item as a string
         """
-        return self.media_type
+        return self.location
 
 
 class Edition(db.Model):
@@ -61,5 +67,28 @@ class Edition(db.Model):
         """
         Represent each item as a string
         """
-        return self.media_type
+        return self.edition
 
+
+class Movielookup(db.Model):
+    """
+    Schema for the movie lookup table.
+    """
+    movie_lookup_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+            "user.user_id", ondelete="CASCADE"), nullable=False)
+    movie_id = db.Column(db.String(100), nullable=False)
+    imdbID = db.Column(db.String(50), nullable=True)
+    movie_title = db.Column(db.String(260), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey(
+            "location.location_id", ondelete="CASCADE"), nullable=False)
+    media_type_id = db.Column(db.Integer, db.ForeignKey(
+            "media.media_type_id", ondelete="CASCADE"), nullable=False)
+    edition_id = db.Column(db.Integer, nullable=False)
+    has_viewed = db.Column(db.Boolean, default=False, nullable=False)
+
+    def __repr__(self):
+        """
+        represent each item as a string
+        """
+        return f"# {self.movie_lookup_id} |Movie title: {self.movie_title} | Location: {self.location_id} | Media type: {self.media_type_id} | Edition: {self.edition_id} | Viewed: {self.has_viewed}"
