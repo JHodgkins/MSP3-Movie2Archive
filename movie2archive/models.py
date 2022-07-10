bbfdf7772f2bb232fea33d1228e7e159bf2cd712
JHodgkins <jhodgkins78@gmail.com>
2022-07-10 14:48:40 +0000

    code: edition schema constructed and added

--- movie2archive/models.py
+++ movie2archive/models.py
@@ -5,7 +5,9 @@ from movie2archive import db
 
 
 class User(db.Model):
-    """ Schema for the user table. """
+    """
+    Schema for the user table.
+    """
     user_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
@@ -14,25 +16,50 @@ class User(db.Model):
     join_date = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
-        """ represent each item as a string """
+        """
+        represent each item as a string
+        """
         return f"#{self.user_id} | Username: {self.username} | Fistname: {self.first_name} | Lastname: {self.last_name} | Join date: {self.join_date} "
 
 
 class MediaType(db.Model):
-    """ Schema for the media type table. """
+    """
+    Schema for the media type table.
+    """
     media_type_id = db.Column(db.Integer, primary_key=True)
     media_type = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        """ Represent each item as a string """
+        """
+        Represent each item as a string
+        """
         return self.media_type
 
 
 class Location(db.Model):
-    """ Schema for the location table. """
+    """
+    Schema for the location table.
+    """
     location_id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        """ Represent each item as a string """
+        """
+        Represent each item as a string
+        """
         return self.media_type
+
+
+class Edition(db.Model):
+    """
+    Schema for the edition table.
+    """
+    edition_id = db.Column(db.Integer, primary_key=True)
+    edition = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        """
+        Represent each item as a string
+        """
+        return self.media_type
+
