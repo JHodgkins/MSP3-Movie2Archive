4b906df5a08f0b1e38d68d2955bad60371667ca3
JHodgkins <jhodgkins78@gmail.com>
2022-07-12 05:25:42 +0000

    fix: temporary disabled PG switch statement

--- movie2archive/__init__.py
+++ movie2archive/__init__.py
@@ -19,10 +19,11 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # Heroku
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    # uri = os.environ.get("DATABASE_URL")
+    # if uri.startswith("postgres://"):
+    #     uri = uri.replace("postgres://", "postgresql://", 1)
+    # app.config["SQLALCHEMY_DATABASE_URI"] = uri  # Heroku
 
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
