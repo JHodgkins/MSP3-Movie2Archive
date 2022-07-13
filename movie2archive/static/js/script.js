859cf797d6e6064ecacd764c521cb414444342a8
JHodgkins <jhodgkins78@gmail.com>
2022-07-13 12:15:20 +0000

    code: mobile toggle and footer date added

--- movie2archive/static/js/script.js
+++ movie2archive/static/js/script.js
@@ -1,1 +1,9 @@
-console.log("Testing js file connection")
+document.addEventListener('DOMContentLoaded', function() {
+    // Navbar initialization
+      let sidenav = document.querySelectorAll('.sidenav');
+      M.Sidenav.init(sidenav);
+});
+
+// Show date on footer
+let date = new Date();
+document.getElementById("copyright").innerHTML = date.getFullYear();
