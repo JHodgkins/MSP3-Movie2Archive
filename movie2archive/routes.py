d968bb5fbed06204ddec671bee7f4d8be30d8621
JHodgkins <jhodgkins78@gmail.com>
2022-07-19 13:15:36 +0000

    code: login, register and logout routes added

--- movie2archive/routes.py
+++ movie2archive/routes.py
@@ -1,45 +1,69 @@
-"""
-Import flask to habdle routes and render_template to handle template files, flash for message feedback, session to hold user session data and url_for for retrieving base url.
-Import Bson so Mongo Id can be read.
-Import werkzeug for security measures applied to user account.
-Import app to launch, db, mongo from __init__ file for database
-details annd connections.
-Import Users, MediaType, Location, Edition, Movielookup from the models file.
-"""
-from flask import (
-    Flask, render_template, flash, session, url_for)
-from bson.objectid import ObjectId
+from flask import render_template, url_for, flash, redirect, request
 from movie2archive import app, db, mongo, bcrypt
-from movie2archive.models import (
-    User, Media, MovieLookup)
+from movie2archive.forms import RegistrationForm, LoginForm
+from movie2archive.models import User, Movielookup, Media
+from flask_login import (
+    login_user, logout_user, current_user, login_required)
 
 
 @app.route("/")
+@app.route("/home")
 def home():
-    """ render a landing page for when user visits Movie2Archive."""
-    return render_template("index.html", )
+    return render_template('index.html')
 
 
 @app.route("/about")
 def about():
-    """ render a landing page for when user visits Movie2Archive."""
-    return render_template('about.html', title='About') 
+    return render_template('about.html', title='About')
 
 
-@app.route("/login")
-def login():
-    """ render a landing page for when user visits Movie2Archive."""
-    return render_template('login.html', title='Login') 
-
-
-@app.route("/register")
+@app.route("/register.html", methods=['POST', 'GET'])
 def register():
-    """ render a landing page for when user visits Movie2Archive."""
-    return render_template('register.html', title='Register') 
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashedpw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashedpw)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Hey {form.username.data} Your account was created sucessfully,\nYou can now log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(
+            user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_redirect = request.args.get('next')
+            return redirect(next_redirect) if next_redirect else redirect(
+                url_for('home'))
+        else:
+            flash(f'login failed, Please try again', 'danger')
+    return render_template("login.html", title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/profile")
+@login_required
+def profile():
+    return render_template("profile.html", title='Profile')
 
 # testing mongo connection
-@app.route("/get_movies")
-def get_movies():
+@app.route("/collection")
+@login_required
+def collection():
     movies = mongo.db.movies.find()
-    return render_template("collection.html", movies=movies)
+    return render_template("collection.html", title='Profile', movies=movies)
