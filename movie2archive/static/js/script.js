f2d07e68db1d661bdb7669bd52429c0b814e2963
JHodgkins <jhodgkins78@gmail.com>
2022-07-24 17:04:51 +0000

    code: added character counter to movie lenghts textarea

--- movie2archive/static/js/script.js
+++ movie2archive/static/js/script.js
@@ -4,6 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
       M.Sidenav.init(sidenav);
 });
 
+function textareaLengthCheck(el) {
+  let textArea = el.value.length;
+  let charactersLeft = 100 - textArea;
+  let count = document.getElementById('lblRemainingCount');
+  count.innerHTML = "Remaining characters: " + charactersLeft;
+}
+
 // Show date on footer
 let date = new Date();
 document.getElementById("copyright").innerHTML = date.getFullYear();
