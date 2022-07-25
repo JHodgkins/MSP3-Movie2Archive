ea14661b174d16d48a45bc5a4d04f04bb33436f4
JHodgkins <jhodgkins78@gmail.com>
2022-07-25 00:33:30 +0000

    code: add, edit and delete edition routes added

--- movie2archive/routes.py
+++ movie2archive/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, url_for, flash, redirect, request
 from movie2archive import app, db, mongo, bcrypt, access_key
 from movie2archive.forms import (
     RegistrationForm, LoginForm, MediaCatForm, EditMediaCatForm,
-    EditLocationCatForm, LocationCatForm, MovieForm)
-from movie2archive.models import User, Movielookup, Media, Location
+    EditLocationCatForm, LocationCatForm, EditionCatForm, EditEditionCatForm, MovieForm)
+from movie2archive.models import User, Movielookup, Media, Location, Edition
 from flask_login import (
     login_user, logout_user, current_user, login_required)
 import urllib
@@ -75,6 +75,7 @@ def collection_all():
     movie_types = Media.query.order_by(Media.type.asc()).all()
     return render_template("collection_all.html", title='My collection', default='no movies currently in your collection', movies=movies, movie_types=movie_types)
 
+
 @app.route("/collection/<int:media_type_id>/")
 @login_required
 def collection_cat(media_type_id):
@@ -110,7 +111,8 @@ def dashboard():
         return redirect(url_for('home'))
     media_types = Media.query.all()
     location_types = Location.query.all()
-    return render_template("dashboard.html", title='Site Dashboard', location_types=location_types, media_types=media_types)
+    edition_types = Edition.query.all()
+    return render_template("dashboard.html", title='Admin Dashboard | Movie2Archive', location_types=location_types, media_types=media_types, edition_types=edition_types)
 
 
 @app.route("/dashboard/add_media_category", methods=['GET', 'POST'])
@@ -231,6 +233,59 @@ def delete_location_cat(location_type_id):
         return redirect(url_for('dashboard'))
 
 
+@app.route("/dashboard/add_edition_category", methods=['GET', 'POST'])
+@login_required
+def add_edition_cat():
+    # Defensive check
+    if str(current_user.id) != access_key:
+        flash(f'{current_user.username}, You are not authorised. to access this url.', 'warning')
+        return redirect(url_for('home'))
+    else:
+        form = EditionCatForm()
+        if form.validate_on_submit():
+            edition_type = Edition(edition=form.edition.data)
+            db.session.add(edition_type)
+            db.session.commit()
+            flash(f'Edition category was added sucessfully to the database!', 'info')
+            return redirect(url_for('dashboard'))
+    return render_template("add_edition_category.html", title='Add edition type', form=form)
+
+
+@app.route("/dashboard/edit_edition_category/<int:edition_type_id>/", methods=['GET', 'POST'])
+@login_required
+def edit_edition_cat(edition_type_id):
+    # Defensive check
+    if str(current_user.id) != access_key:
+        flash(f'{current_user.username}, You are not authorised. to access this url.', 'warning')
+        return redirect(url_for('home'))
+    else:
+        edition_types = Edition.query.get_or_404(edition_type_id)
+        form = EditEditionCatForm()
+        if form.validate_on_submit():
+            edition_types.edition = form.edition.data
+            db.session.commit()
+            flash(f'Edition category was sucessfully updated and added to the database!', 'info')
+            return redirect(url_for('dashboard'))
+        elif request.method == 'GET':
+            form.edition.data = edition_types.edition
+    return render_template("edit_edition_category.html", title='Edit edition Category', form=form)
+
+
+@app.route("/dashboard/delete_edition_category<int:edition_type_id>/", methods=['GET', 'POST'])
+@login_required
+def delete_edition_cat(edition_type_id):
+    # Defensive check
+    if str(current_user.id) != access_key:
+        flash(f'{current_user.username}, You are not authorised. to access this url.', 'warning')
+        return redirect(url_for('home'))
+    else:
+        edition_types = Edition.query.get_or_404(edition_type_id)
+        db.session.delete(edition_types)
+        db.session.commit()
+        flash(f'Edition category was sucessfully deleted from the database!', 'info')
+        return redirect(url_for('dashboard'))
+
+
 @app.route("/my_collection/add_movie/", methods=['GET', 'POST'])
 @login_required
 def add_movie():
@@ -240,15 +295,19 @@ def add_movie():
     media_list = [(media.id, media.type) for media in media_types]
     location_types = Location.query.all()
     location_list = [(location.id, location.location) for location in location_types]
+    edition_types = Edition.query.all()
+    edition_list = [(edition.id, edition.edition) for edition in edition_types]
     form = MovieForm()
     form.media_id.choices = media_list
     form.location_id.choices = location_list
+    form.edition_id.choices = edition_list
     if form.validate_on_submit():
         movie = Movielookup(
             title=form.title.data,
             notes=form.notes.data,
             media_id=form.media_id.data,
             location_id=form.location_id.data,
+            edition_id=form.edition_id.data,
             user_id=current_user.id
         )
         db.session.add(movie)
